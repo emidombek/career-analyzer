0c2e08cfc91ba11af04e1df6d241960f3fed767b
emidombek <emilia.maria.dombek@gmail.com>
2023-08-16 23:57:29 +0100

    Reformat code layout

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import asciichartpy
-import datetime  # Import the datetime module
+import datetime
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -9,10 +9,8 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive",
 ]
 
-# Load the service account credentials from the JSON file
 CREDS = Credentials.from_service_account_file("creds.json")
 
-# Authorize the credentials with the required scopes
 SCOPED_CREDS = CREDS.with_scopes(
     [
         "https://www.googleapis.com/auth/spreadsheets",
@@ -21,10 +19,8 @@ SCOPED_CREDS = CREDS.with_scopes(
     ]
 )
 
-# Authorize the gspread client with the scoped credentials
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
-# Open the Google Spreadsheet by its title
 SHEET = GSPREAD_CLIENT.open("career_analyzer")
 
 
@@ -58,7 +54,7 @@ class Survey:
             "Please select your career area:",
             "On a scale of 1 to 5, how satisfied are you with your career?",
             "Are you considering a career change? (yes/no)",
-            "If yes, what factors are influencing your decision? (comma-separated)",
+            "If yes, what factors are influencing your decision?",
             "Do you prefer remote work? (yes/no)",
         ]
         self.career_areas = [
@@ -67,6 +63,13 @@ class Survey:
             "Finance",
             "Education",
             "Other",
+        ]
+        self.career_satisfaction_ratings = [
+            "Unhappy",
+            "Unsatisfied",
+            "Somewhat Satisfied",
+            "Happy",
+            "Dreamjob",
         ]
         self.career_change_factors = [
             "Work-life balance",
@@ -80,21 +83,17 @@ class Survey:
 
     def add_timestamp(self):
         try:
-            # Get the current timestamp
             timestamp = datetime.datetime.now()
-
-            # Convert the timestamp to a string
             timestamp_str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-
-            # Add the timestamp to the answers list
             self.answers.insert(0, timestamp_str)
-
             print("Timestamp added to the survey answers.")
         except Exception as e:
             print("Error adding timestamp:", str(e))
 
     def conduct_survey(self):
+        self.answers = []  # Reset the answers list
         print("Welcome to the survey!")
+        self.add_timestamp()
         for i, question in enumerate(self.questions):
             if i == 2:
                 print(f"{question} (Select a number)")
@@ -106,10 +105,23 @@ class Survey:
                     answer = self.career_areas[choice_idx]
                 except (ValueError, IndexError):
                     answer = "Invalid choice"
+            elif i == 3:  # Career satisfaction rating
+                print("Select satisfaction rating:")
+                for idx, factor in enumerate(self.career_satisfaction_ratings, start=1):
+                    print(f"{idx}. {factor}")
+                choices = input("Your choice(s): ").split()
+                selected_factors = []
+                try:
+                    for choice in choices:
+                        choice_idx = int(choice) - 1
+                        selected_factors.append(
+                            self.self.career_satisfaction_ratings[choice_idx]
+                        )
+                    answer = ", ".join(selected_factors)
+                except (ValueError, IndexError):
+                    answer = "Invalid choice"
             elif i == 5:  # Career change factors question
-                print(
-                    "Select career change factors (Enter the number(s) separated by spaces):"
-                )
+                print("Select career change factors:")
                 for idx, factor in enumerate(self.career_change_factors, start=1):
                     print(f"{idx}. {factor}")
                 choices = input("Your choice(s): ").split()
@@ -129,15 +141,9 @@ class Survey:
 
     def store_results_in_google_sheet(self):
         try:
-            # Open the first sheet of the Google Spreadsheet
             worksheet = SHEET.get_worksheet(0)
-
-            # Convert the survey answers into a list of lists for each row
             values = [self.answers]
-
-            # Append the values to the Google Sheet
             worksheet.append_rows(values)
-
             print("Survey results stored in Google Sheet.")
         except Exception as e:
             print("Error storing survey results:", str(e))
@@ -156,33 +162,27 @@ class Survey:
             print()
 
     def view_survey_statistics(self):
-        # Count the occurrences of each answer
         answer_counts = {}
         total_responses = len(self.answers)
-
         for answer in self.answers:
             if answer in answer_counts:
                 answer_counts[answer] += 1
             else:
                 answer_counts[answer] = 1
 
-        # Calculate percentages
         percentages = {
             answer: count / total_responses * 100
             for answer, count in answer_counts.items()
         }
 
-        # Display the statistics using ASCII bars
         print("\nSurvey Result Statistics:")
         for answer, percentage in percentages.items():
             print(f"{answer}: {'#' * int(percentage)} ({percentage:.2f}%)")
 
+    @staticmethod
     def main():
         display_welcome()
-
-        # Create a survey instance
         survey = Survey()
-
         while True:
             display_main_menu()
             choice = input("Enter your choice: ")
@@ -193,7 +193,6 @@ class Survey:
                 survey.store_results_in_google_sheet()
                 survey.display_results()
             elif choice == "2":
-                # View survey result statistics
                 survey.view_survey_statistics()
             elif choice == "3":
                 print("Goodbye!")
