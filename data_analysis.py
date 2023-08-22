c07a592e2fd3e02d6ec5395fd7bbd8ace9230d38
emidombek <emilia.maria.dombek@gmail.com>
2023-08-22 13:22:35 +0100

    Add colored colors and emoji for bars

--- data_analysis.py
+++ data_analysis.py
@@ -1,11 +1,11 @@
 from survey import get_google_sheet_client, column_mapping
-from termcolor import colored, cprint
+from termcolor import colored
 
 SHEET = get_google_sheet_client(creds_file="creds.json", sheet_name="career_analyzer")
 
 # Emoji definitions
-BORDER = ":white_large_square:"
-BAR = ":blue_square:"
+BORDER = "□"
+BAR = "🟦"
 
 
 class DataAnalyzer:  # Class that analyzes survey data stored in the googlespread
@@ -17,31 +17,38 @@ class DataAnalyzer:  # Class that analyzes survey data stored in the googlesprea
             worksheet = SHEET.get_worksheet(0)
             data = worksheet.get_all_records()
 
-            # Count unique timestamps and survey responses
-            unique_timestamps = set()
-            for row in data:
-                timestamp = row["Timestamp"]
-                if timestamp.startswith("20"):
-                    unique_timestamps.add(timestamp)
-            survey_count = len(unique_timestamps)
-            print(f"\n{Colors.HEADER}Count of Surveys: {survey_count}{Colors.ENDC}")
+            # Count timestamps and survey responses
+            unique_timestamps = set()  # Reset variable
+            for row in data:  # Loop through Timestamp data
+                timestamp = row["Timestamp"]  # Map to spreadsheet row
+                if timestamp.startswith("20"):  # Check if entry is timestamp
+                    unique_timestamps.add(timestamp)  # Add data
+            survey_count = len(unique_timestamps)  # Count timestamps
+            survey_count_text = colored(f"Count of Surveys: {survey_count}", "cyan")
+            print(survey_count_text)
 
-            total_age = 0
-            age_count = 0
-            for row in data:
+            # Average age calculation
+            total_age = 0  # Set to 0
+            age_count = 0  # Set to 0
+            for row in data:  # Loop through survey data defined in column_mapping
                 age = row[column_mapping["How old are you?"]]
                 try:
-                    age = int(age)
-                    total_age += age
-                    age_count += 1
+                    age = int(age)  # Can age be converted to integer
+                    total_age += age  # All of the ages added together
+                    age_count += 1  # Number of entries into the age column
                 except ValueError:
                     pass
 
-            if age_count > 0:
-                average_age = total_age / age_count
-                print(
-                    f"{Colors.OKGREEN}Average Age of Survey Participant: {average_age:.2f}{Colors.ENDC}"
+            if (
+                age_count > 0
+            ):  # Checks entries before performing calculation to avoid divding by zero
+                average_age = (
+                    total_age / age_count
+                )  # Total sum of ages divided by total entries
+                avg_age_text = colored(
+                    f"Average Age of Survey Participant: {average_age:.2f}", "green"
                 )
+                print(avg_age_text)
 
             # Create bar charts for questions (excluding name and age)
             for i, question in enumerate(column_mapping.keys()):
@@ -59,16 +66,18 @@ class DataAnalyzer:  # Class that analyzes survey data stored in the googlesprea
                             answer_counts[answer] = 1
 
                     if question not in ["What is your name?", "How old are you?"]:
-                        print(
-                            f"\n{Colors.HEADER}Statistics for Question: {question}{Colors.ENDC}"
+                        header_text = colored(
+                            f"Statistics for Question: {question}", "cyan"
                         )
                         for answer, count in answer_counts.items():
                             percentage = (count / total_responses) * 100
-                            bar = "#" * int(percentage)
-                            print(
-                                f"{Colors.OKBLUE}{answer}:{Colors.OKGREEN} {bar} ({percentage:.2f}%)"
-                            )
+                            bar_emoji = "🟦" * int(
+                                percentage / 10
+                            )  # Using a blue square emoji
+                            percentage_text = colored(f"({percentage:.2f}%)", "green")
+                            print(f"{answer}:{bar} {percentage_text}")
                         print()
 
         except Exception as e:
-            print(f"{Colors.FAIL}Error analyzing survey data: {str(e)}{Colors.ENDC}")
+            error_message = colored(f"Error analyzing survey data: {str(e)}", "red")
+            print(error_message)
