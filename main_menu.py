66253128cb4b97eae599af6be3b49e60b85b4709
emidombek <emilia.maria.dombek@gmail.com>
2023-08-22 11:06:35 +0100

    Add function to clear the console

--- main_menu.py
+++ main_menu.py
@@ -1,7 +1,12 @@
 from constants import Colors
 from survey import Survey
-from data_analysis import DataAnalyzer
+from data_analysis import DataAnalyzer, column_mapping
 from simple_term_menu import TerminalMenu  # Import the TerminalMenu class
+import os
+
+
+def clear_console():  # function to clear the console
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 class TerminalMenuApp:
@@ -15,23 +20,31 @@ class TerminalMenuApp:
         self.menu = TerminalMenu(self.menu_items)
 
     def take_survey(self):  # Runs survey
+        clear_console()
         survey = Survey()
         survey.conduct_survey()
         survey.store_results_in_google_sheet()
         survey.display_results()
 
     def view_statistics(self):  # View stats from survey
-        data_analyzer = DataAnalyzer()
+        clear_console()
+        data_analyzer = DataAnalyzer(column_mapping)  # Pass column_mapping here
         data_analyzer.view_survey_statistics()
 
-    def about(self):  # About with placeholder text will be changed later
+    def about(self):  # About text
+        clear_console()
         about_text = """ 
-        Career Analyzer - Terminal Menu
+        Career Analyzer - About 
 
-        This program allows you to take a survey about your career and view statistics
-        about the survey results.
+        This is a Python based terminal programme, 
+        that aims to gauge Career Satisfaction and the likelyhood of a career or job change happening as a result. 
+        There is an option to complete a survey under 'Take Survey'. 
+        The survey asks about job satisfaction and the driving factors behind a potential change in employment 
+        as well as preferences in regareds to remote work.
+        The data is stored in a google spreadsheet and analyzed.
+        Basic statistics such as count of surveys, average age of participant and answer percentages can be viewed via the 'View Statistics' option. 
 
-        Created by Your Name 
+        Created by Emilia Dombek
 
         """
         print(Colors.OKBLUE + about_text + Colors.ENDC)
@@ -47,6 +60,7 @@ class TerminalMenuApp:
             elif selected_index == 2:
                 self.about()
             elif selected_index == 3:
+                clear_console()
                 print(Colors.OKGREEN + "Thank You!" + Colors.ENDC)
                 print(Colors.OKGREEN + "Goodbye!" + Colors.ENDC)
                 break
